aba2c7bd68c507ed75aea7d687fc5ad5b6853472
xionghl <1445695649@qq.com>
2021-08-26 15:02:41 +0800

    feat:  1. 两数之和  by xionghl

--- interview/HELP.md
+++ interview/HELP.md
@@ -9,3 +9,14 @@ For further reference, please consider the following sections:
 * [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.5.4/maven-plugin/reference/html/#build-image)
 * [Spring Boot DevTools](https://docs.spring.io/spring-boot/docs/2.5.4/reference/htmlsingle/#using-boot-devtools)
 
+LeetCode 热题 HOT 100  https://leetcode-cn.com/problem-list/2cktkvj/
+
+精选 100 道力扣（LeetCode）上最热门的题目，适合初识算法与数据结构的新手和想要在短时间内高效提升的人，
+熟练掌握这 100 道题，你就已经具备了在代码世界通行的基本能力。
+
+如果成为大神有什么诀窍的话，那就是相信时间的力量，每天进步一点就够了
+
+1.01^365 = 37.8
+0.99^365 = 0.03
+
+2021-08-26
---
+++ interview/src/main/java/com/xionghl/interview/one/Solution.java
@@ -0,0 +1,51 @@
+package com.xionghl.interview.one;
+
+import java.util.Arrays;
+
+/**
+ * @author xionghaolin
+ * 1. 两数之和
+ * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ */
+public class Solution {
+
+    /**
+     * @param nums   整数数组
+     * @param target 整数目标值
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1 ; j <nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(Solution.twoSum(nums, target)));
+    }
+}
